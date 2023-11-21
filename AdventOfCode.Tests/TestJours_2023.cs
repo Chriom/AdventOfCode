5eb69edf5a67b8a219f2b04d977fa610d6f68f7c
Christophe Morisset <morisset.chris@gmail.com>
2023-11-21 13:45:58 +0100

    Prepa

---
+++ AdventOfCode.Tests/TestJours_2023.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCode.Helpers;
+
+namespace AdventOfCode.Tests
+{
+    [TestClass]
+    public class TestJours_2023
+    {
+        public TestJours_2023()
+        {
+            EntreesHelper.EstEnmodeTest = true;
+            EntreesHelper.Numero = 1;
+        }
+    }
+}
